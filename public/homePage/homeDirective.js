bd0e3fa3ee4c1647a13af8def9eb01f9cfbd5f6a
juan Hernandez <juan.hernandeznoriega01@utrgv.edu>
2017-07-13 20:02:57 -0500

    fixed  marker issue

--- public/homePage/homeDirective.js
+++ public/homePage/homeDirective.js
@@ -5,11 +5,11 @@ angular.module('app').directive('homeDirective', function() {
 
         },
         controller: function($scope, $timeout, googleService, userService) {
-            var map;
-            var mapElement;
-            var center;
-            var buttonArray = [];
-            var markerArray = [];
+            let map;
+            let mapElement;
+            let center;
+            let buttonArray = [];
+            let markerArray = [];
 
 
 
@@ -52,26 +52,26 @@ angular.module('app').directive('homeDirective', function() {
                 googleService.getPlaces(center)
                     .then(function(response) {
                         console.log(response);
-                        var infowindows = [];
-                        var results = response.data;
-                        for (var result of results) {
+                        let infowindows = [];
+                        let results = response.data;
+                        for (let result of results) {
 
-                            var div = document.createElement('div');
-                            var line1 = document.createElement('h6');
+                            let div = document.createElement('div');
+                            let line1 = document.createElement('h6');
                             line1.innerText = result.name;
-                            var line2 = document.createElement('h6');
+                            let line2 = document.createElement('h6');
                             line2.innerText = result.vicinity;
-                            var line3 = document.createElement('p');
+                            let line3 = document.createElement('p');
                             line3.innerText = 'Ballers ' + result.count;
-                            var button = document.createElement('button');
-                            var text = document.createTextNode('Check in');
+                            let button = document.createElement('button');
+                            let text = document.createTextNode('Check in');
                             button.appendChild(text);
                             button.dataset.id = result.id;
 
                             button.addEventListener('click', function(e) {
-                                var id = e.target.dataset.id;
-                                var text = e.target.parentElement.children[2];
-                                var num = parseInt(text.innerText.split(' ')[1]);
+                                let id = e.target.dataset.id;
+                                let text = e.target.parentElement.children[2];
+                                let num = parseInt(text.innerText.split(' ')[1]);
 
 
                                 if (e.target.innerText === "Check in") {
@@ -79,7 +79,7 @@ angular.module('app').directive('homeDirective', function() {
                                     checkIn(id);
                                     num++;
                                     for (let i = 0; i < buttonArray.length; i++) {
-                                        var btn = buttonArray[i];
+                                        let btn = buttonArray[i];
                                         btn.disabled = true;
                                         if (btn.dataset.id === id) {
                                             console.log(markerArray[i]);
@@ -92,7 +92,7 @@ angular.module('app').directive('homeDirective', function() {
                                     checkOut(id);
                                     num--;
                                     for (let i = 0; i < buttonArray.length; i++) {
-                                        var btnEnd = buttonArray[i];
+                                        let btnEnd = buttonArray[i];
                                         btnEnd.disabled = false;
                                         if (btnEnd.dataset.id === id) {
                                             markerArray[i].setIcon();
@@ -110,14 +110,14 @@ angular.module('app').directive('homeDirective', function() {
                             div.appendChild(line2);
                             div.appendChild(line3);
                             div.appendChild(button);
-                            var image = result.icon;
-                            var infowindow = new google.maps.InfoWindow({
+                            let image = result.icon;
+                            let infowindow = new google.maps.InfoWindow({
                                 content: div
 
 
                             });
                             infowindows.push(infowindow);
-                            var marker = new window.google.maps.Marker({
+                            let marker = new window.google.maps.Marker({
                                 map: map,
                                 position: {
                                     lat: result.geometry.location.lat,
@@ -138,25 +138,46 @@ angular.module('app').directive('homeDirective', function() {
                     })
 
             }
-            navigator.geolocation.getCurrentPosition(function(position) {
-                console.log(position);
-                if (position.coords.latitude !== center.lat && position.coords.longtitude !== center.lng) {
-                    center = {lat: position.coords.latitude, lng: position.coords.longitude};
-                    map.panTo(center);
-                    getPlaces(center);
-                }
+
+            if (navigator && navigator.geolocation && navigator.geolocation.getCurrentPosition) {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    console.log(position);
+                    if (position.coords.latitude !== center.lat && position.coords.longtitude !== center.lng) {
+                        center = {lat: position.coords.latitude, lng: position.coords.longitude};
+                        map.panTo(center);
+                        getPlaces(center);
+                    }
 
 
-            });
+                });
+            }
+
             $timeout(function() {
+
                 mapElement = document.getElementById('map');
 
-                center = {lat: 34.7776825, lng: -96.79572639999999
+                center = {lat: 30.307182, lng: -97.755996
                 };
+
+
                 map = new window.google.maps.Map(mapElement, {
                     center: center,
                     zoom: 11
                 });
+
+                // var input = document.getElementById('pac-input');
+                // var searchBox = new google.maps.places.SearchBox(input);
+                // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+                //
+                // // Bias the SearchBox results towards current map's viewport.
+                // map.addListener('bounds_changed', function() {
+                //     searchBox.setBounds(map.getBounds());
+                // });
+
+
+
+
+
 
                 getPlaces(center);
 
